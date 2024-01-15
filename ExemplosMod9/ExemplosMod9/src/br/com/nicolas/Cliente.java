4b66fc51e47e7db224537b9cfa8d35ae9b4667df
Nícolas Santos <nicollas.developer@gmail.com>
2024-01-15 17:25:58 -0300

    aula 01 mod 9

---
+++ ExemplosMod9/ExemplosMod9/src/br/com/nicolas/Cliente.java
@@ -0,0 +1,28 @@
+package br.com.nicolas;
+
+
+/**
+ * @author nicolas.santos
+ *
+ */
+public class Cliente {
+
+    private Integer codigo;
+
+    private Long codigoMaior = 212112l;
+
+    private Double valorDecimal1 = 10.1;
+
+    private Boolean status = true;
+
+    private Float valorDecimal = 10.0f;
+
+    private Short shor;
+
+    private Byte bi;
+
+    private Character letra = 'A';
+
+    public static void main(String args[]){
+    }
+}
