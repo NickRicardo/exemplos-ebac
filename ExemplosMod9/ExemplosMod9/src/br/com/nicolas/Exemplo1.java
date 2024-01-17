8d26ee172066cea135decf48af14cd6998bc2146
Nícolas Santos <nicollas.developer@gmail.com>
2024-01-17 17:38:18 -0300

    Iniciando aula sobre casting

---
+++ ExemplosMod9/ExemplosMod9/src/br/com/nicolas/Exemplo1.java
@@ -0,0 +1,23 @@
+package br.com.nicolas;
+
+public class Exemplo1 {
+    public static void main(String args[]){
+
+        Boolean status = true;
+
+        Boolean status2 = Boolean.TRUE;
+
+        Character c = 'A';
+
+        Integer idade = 10;
+
+        Long cpf = Long.valueOf(123123);
+
+        //unboxing
+        boolean status3 = Boolean.TRUE;
+
+        char letra = Character.valueOf('A');
+
+        int indade2 = Integer.valueOf(12);
+    }
+}
